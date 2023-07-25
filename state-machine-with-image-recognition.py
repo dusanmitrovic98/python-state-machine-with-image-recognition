22fc6b5b2ed394acf3ba5ee1cd9925d9b577a328
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-25 14:47:03 +0200

    Update state-machine-with-image-recognition.py

--- state-machine-with-image-recognition.py
+++ state-machine-with-image-recognition.py
@@ -29,3 +29,4 @@ def find_and_click(image_path):
         screen_width, screen_height = pyautogui.size()
 
         # Take a screenshot of the screen and convert it to grayscale
+        screenshot = pyautogui.screenshot(region=(0, 0, screen_width, screen_height))

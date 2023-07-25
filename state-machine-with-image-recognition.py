bd53e0d0015e513e9b0abf8240a321621d14e51b
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-25 14:53:08 +0200

    Update state-machine-with-image-recognition.py

--- state-machine-with-image-recognition.py
+++ state-machine-with-image-recognition.py
@@ -44,3 +44,4 @@ def find_and_click(image_path):
             x, y = max_loc
 
             # Move the mouse to the center of the matched image and click
+            x += target_image.shape[1] // 2

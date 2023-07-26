200a2dfb96140f75d6d1c2862438ab46aa63c923
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-26 20:14:46 +0200

    Update state-machine-with-image-recognition.py

--- state-machine-with-image-recognition.py
+++ state-machine-with-image-recognition.py
@@ -99,3 +99,4 @@ def automate_states(states):
 
         # Find the current state based on the ID
         current_state = next((state for state in states if state["id"] == current_state_id), None)
+

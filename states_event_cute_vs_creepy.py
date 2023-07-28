5732f9c7816f30dec01dd3fd31626322c020344d
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-28 20:48:13 +0200

    Update

--- states_event_cute_vs_creepy.py
+++ states_event_cute_vs_creepy.py
@@ -8,6 +8,7 @@ FREQUENCY = 1
 current_directory = os.path.dirname(os.path.abspath(__file__))
 img_ecvc_01 = os.path.join(os.path.join(current_directory, "images\\event_cute_vs_creepy\\img_ecvc_01.jpg"))
 img_ecvc_02 = os.path.join(os.path.join(current_directory, "images\\event_cute_vs_creepy\\img_ecvc_02.jpg"))
+img_ecvc_03 = os.path.join(os.path.join(current_directory, "images\\event_cute_vs_creepy\\img_ecvc_03.jpg"))
 
 STATES = [
     State(
@@ -22,7 +23,14 @@ STATES = [
         image_paths=[img_ecvc_02],
         actions=[click],
         durations=[FREQUENCY],
-        next_states=[999]
+        next_states=[2]
+    ),
+    State(
+        id=2,
+        image_paths=[img_ecvc_03],
+        actions=[click],
+        durations=[FREQUENCY / 4],
+        next_states=[3]
     ),
     State( # ! end state should be removed
         id=999,

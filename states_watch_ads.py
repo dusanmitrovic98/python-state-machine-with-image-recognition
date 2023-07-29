09046b783491d1fd57abfeff0752f48ab36a2203
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-29 15:58:38 +0200

    Update

--- states_watch_ads.py
+++ states_watch_ads.py
@@ -11,32 +11,39 @@ from state import State
 
 STATES = [
     State(
-        id=10,
+        id=10, # ! shop button
         image_paths=[img_wa_10],
         actions=[click],
         durations=[FREQUENCY],
         next_states=[11]
     ),
     State(
-        id=11,
+        id=11, # ! gold button
         image_paths=[img_wa_11],
         actions=[click],
         durations=[FREQUENCY],
         next_states=[12]
     ),
     State(
-        id=12,
+        id=12, # ! scroll
         image_paths=[img_wa_12],
         actions=[held_mouse_up],
         durations=[FREQUENCY],
         next_states=[13]
     ),
     State(
-        id=13,
+        id=13, # ! watch ads button clicked
         image_paths=[img_wa_13],
         actions=[click],
         durations=[FREQUENCY],
-        next_states=[999]
+        next_states=[14]
+    ),
+    State(
+        id=13, # ! x1 button clicked
+        image_paths=[img_wa_14],
+        actions=[click],
+        durations=[FREQUENCY],
+        next_states=[13]
     ),
     # State( # ! end state should be removed
     #     id=999,
